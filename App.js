514b23f7745a607d3f578d0d34e5ba4d1467cdb3
Victoria <victoria.perdomo@ordergroove.com>
2020-10-04 12:19:47 -0300

    Update Router

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import { ImageBackground, View, Text } from "react-native";
 import EventListScreen from "./src/screens/event-list.screen";
 import EventDetailScreen from "./src/screens/event-detail.screen";
@@ -10,10 +10,9 @@ import SignUpScreen from "./src/screens/signup.screen";
 import TermsScreen from "./src/screens/terms.screen";
 import PrivacyScreen from "./src/screens/privacy.screen";
 import PersonalInfoScreen from "./src/screens/personal-info.screen";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import i18n from "./src/locale/i18n";
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 const MyTheme = {
   ...DefaultTheme,
@@ -25,100 +24,100 @@ const MyTheme = {
 };
 
 export default function App() {
-  const Drawer = createDrawerNavigator();
   return (
-    <NavigationContainer theme={MyTheme} style={{ backgroundColor: MyTheme.colors.primary }}>
-      <Drawer.Navigator initialRouteName="LogIn" screenOptions={{ backgroundColor: MyTheme.primary }} drawerType="slide">
-        <Drawer.Screen
+
+    <NavigationContainer theme={ MyTheme } style={ { backgroundColor: MyTheme.colors.primary } }>
+      <Stack.Navigator initialRouteName="LogIn" screenOptions={ { backgroundColor: MyTheme.primary } } StackType="slide">
+        <Stack.Screen
           name="LogIn"
-          component={SignInScreen}
-          options={{
-            title: i18n.t("drawer_login"),
+          component={ SignInScreen }
+          options={ {
+            title: i18n.t("Stack_login"),
             header: () => null,
-          }}
+          } }
         />
-        <Drawer.Screen
+        <Stack.Screen
           name="SignUp"
-          component={SignUpScreen}
-          options={{
-            title: i18n.t("drawer_signup"),
+          component={ SignUpScreen }
+          options={ {
+            title: i18n.t("Stack_signup"),
             header: () => null,
-          }}
+          } }
         />
-        <Drawer.Screen
+        <Stack.Screen
           name="PersonalInfo"
-          component={PersonalInfoScreen}
-          options={{
+          component={ PersonalInfoScreen }
+          options={ {
             title: i18n.t("personal_info_title"),
             header: () => null,
-          }}
+          } }
         />
-        <Drawer.Screen
+        <Stack.Screen
           name="Events"
-          component={EventListScreen}
-          options={{
-            title: i18n.t("drawer_events"),
+          component={ EventListScreen }
+          options={ {
+            title: i18n.t("Stack_events"),
             header: () => null,
             // headerTitle: (props) => <LogoTitle {...props} />,
             // headerRight: () => <Button onPress={() => alert("This is a button!")} title="Info" color="#fff" />,
-          }}
+          } }
         />
-        <Drawer.Screen
+        <Stack.Screen
           name="EventDetail"
-          component={EventDetailScreen}
-          options={{
+          component={ EventDetailScreen }
+          options={ {
             title: "Event",
-            header: (props) => <Header {...props} title="EEO Event" />,
-          }}
+            header: (props) => <Header { ...props } title="EEO Event" />,
+          } }
         />
-        <Drawer.Screen
+        <Stack.Screen
           name="Enrolled"
-          component={EnrolledScreen}
-          options={{
+          component={ EnrolledScreen }
+          options={ {
             title: "Enrolled",
-            header: (props) => <Header {...props} title="QR Code" />,
-          }}
+            header: (props) => <Header { ...props } title="QR Code" />,
+          } }
         />
-        <Drawer.Screen
+        <Stack.Screen
           name="Terms"
-          component={TermsScreen}
-          options={{
+          component={ TermsScreen }
+          options={ {
             title: i18n.t("signup_terms_link"),
-            header: (props) => <Header {...props} />,
-          }}
+            header: (props) => <Header { ...props } />,
+          } }
         />
-        <Drawer.Screen
+        <Stack.Screen
           name="Privacy"
-          component={PrivacyScreen}
-          options={{
+          component={ PrivacyScreen }
+          options={ {
             title: i18n.t("signup_privacy_policy"),
-            header: (props) => <Header {...props} />,
-          }}
+            header: (props) => <Header { ...props } />,
+          } }
         />
-      </Drawer.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 function Header(props) {
   return (
-    <ImageBackground style={{ width: "100%", height: 100 }} source={require("./assets/header-degre.png")}>
+    <ImageBackground style={ { width: "100%", height: 100 } } source={ require("./assets/header-degre.png") }>
       <View
-        style={{
+        style={ {
           flexDirection: "column",
           justifyContent: "flex-end",
           height: 100,
-        }}
+        } }
       >
         <Text
-          style={{
+          style={ {
             fontSize: 35,
             color: "white",
             paddingLeft: 5,
             paddingBottom: 15,
-          }}
+          } }
         >
-          {" "}
-          {props.title}{" "}
+          { " " }
+          { props.title }{ " " }
         </Text>
       </View>
     </ImageBackground>
